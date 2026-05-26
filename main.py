1c91a54cc994a298c8661918e65a888eb4078e02
D-A-A-H <radimtuliev@gmail.com>
2026-05-26 10:43:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ HTML_CONTENT = """<!DOCTYPE html>
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <title>Пример простой страницы html</title>
+    <title>Пример страницы html</title>
 </head>
 <body>
-    Я люблю кушать кашу с ягодами :)
+    —
 </body>
 </html>"""
 
@@ -42,34 +42,28 @@ class Feedback(BaseModel):
                 raise ValueError('Использование недопустимых слов')
         return v
 
-# 🌟 Задание 1.1
 @app.get("/")
 def root():
     return {"message": "Добро пожаловать в моё приложение FastAPI!"}
 
-# 🌟 Задание 1.2
 @app.get("/page")
 def html_page():
     return HTMLResponse(content=HTML_CONTENT, status_code=200)
 
-# 🌟 Задание 1.3*
 @app.post("/calculate")
 def calculate(num1: float, num2: float):
     return {"result": num1 + num2}
 
-# 🌟 Задание 1.4
 @app.get("/users")
 def get_user():
     return User(name="Ваше Имя и Фамилия", id=1).model_dump()
 
-# 🌟 Задание 1.5*
 @app.post("/user")
 def check_adult(user: UserAge):
     data = user.model_dump()
     data["is_adult"] = user.age >= 18
     return data
 
-# 🌟 Задание 2.1 & 2.2*
 @app.post("/feedback")
 def submit_feedback(fb: Feedback):
     feedbacks.append(fb.model_dump())
